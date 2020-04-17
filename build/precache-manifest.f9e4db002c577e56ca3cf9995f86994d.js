bdcffa61a4cc040da4309cf56c12059e58893702
George Izraelian <g.izraelian@gmail.com>
2020-04-17 14:31:47 -0400

    removed build from ignore

---
+++ build/precache-manifest.f9e4db002c577e56ca3cf9995f86994d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ee1b4ce56b60989874040a5786c4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00aea9ed5bca86a7ca38",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d50575aa3fc5122750f6",
+    "url": "/static/js/2.9588beaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9588beaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00aea9ed5bca86a7ca38",
+    "url": "/static/js/main.ee3787bd.chunk.js"
+  },
+  {
+    "revision": "4d4c2eac8e6cc53681e7",
+    "url": "/static/js/runtime-main.82249d56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
